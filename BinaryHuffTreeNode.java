fdd1e07ba1f858dcb91a97847ee5eebe7e175332
srinivasu619 <srinivasu.618@gmail.com>
2017-11-09 22:41:44 +0530

    Added Code for the treenode and composite object for storing frequency

---
+++ BinaryHuffTreeNode.java
@@ -0,0 +1,10 @@
+public class BinaryHuffTreeNode {
+    Character data;
+    public int frequency;
+    BinaryHuffTreeNode left,right;
+    public BinaryHuffTreeNode(Character data,int frequency) {
+        // TODO Auto-generated constructor stub
+        this.data=data;
+        this.frequency=frequency;
+    }
+}
---
+++ FrequencyCode.java
@@ -0,0 +1,4 @@
+public class FrequencyCode {
+	int frequency;
+	String code;
+}
